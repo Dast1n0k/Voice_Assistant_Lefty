a2f7e13cb47c8b996c3446191182f876dd565000
dessyaid <malyuitenko@gmail.com>
2022-12-12 03:08:31 +0200

    Add internet connection error

--- DataBase.cs
+++ DataBase.cs
@@ -13,6 +13,7 @@ namespace Lefty
         
         String[] greetings = new String[4] { "Hello", "Hi", "Hi, how are you", "I'm here, how can I help you" };
         String[] heads_or_tails = new String[2] { "tail","head" };
+        String Error = "Lost internet connection";
 
         public String greetings_action()
         {
@@ -24,37 +25,51 @@ namespace Lefty
         public String get_jokes() //joke parse
         {
             string url = "https://official-joke-api.appspot.com/random_joke";
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
-            request.Method = "GET";
 
-            var webResponse = request.GetResponse();
-            var webStream = webResponse.GetResponseStream();
-
-            using (var responseReader = new StreamReader(webStream))
+            try
             {
-                var response = responseReader.ReadToEnd();
-                Joke joke = JsonConvert.DeserializeObject<Joke>(response);
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                request.Method = "GET";
+                var webResponse = request.GetResponse();
+                var webStream = webResponse.GetResponseStream();
 
-                return joke.setup + "\n" + joke.punchline;
+                using (var responseReader = new StreamReader(webStream))
+                {
+                    var response = responseReader.ReadToEnd();
+                    Joke joke = JsonConvert.DeserializeObject<Joke>(response);
+
+                    return joke.setup + "\n" + joke.punchline;
+                }
             }
-
+            catch
+            {
+                return Error;
+            }
         }
         public String get_weather() //weather parse
         {
             string url = "http://api.openweathermap.org/data/2.5/weather?q=Kyiv&units=metric&appid=6fa095c114c44b8983cf448560847507";
 
-            HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
-            HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
-            string response;
-            using (StreamReader streamReader = new StreamReader(httpWebResponse.GetResponseStream()))
+            try
             {
-                response = streamReader.ReadToEnd();
-            }
-            WeatherInfo weatherResponse = JsonConvert.DeserializeObject<WeatherInfo>(response);
+                HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+                HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+                string response;
+                using (StreamReader streamReader = new StreamReader(httpWebResponse.GetResponseStream()))
+                {
+                    response = streamReader.ReadToEnd();
+                }
+                WeatherInfo weatherResponse = JsonConvert.DeserializeObject<WeatherInfo>(response);
 
-            return "In city " + weatherResponse.name + " " + weatherResponse.Weather[0].description + " " + "temperature" + " " + weatherResponse.Main.temp + " "  + "degrees Celsius";
+                return "In city " + weatherResponse.name + " " + weatherResponse.Weather[0].description + " " + "temperature" + " " + weatherResponse.Main.temp + " "  + "degrees Celsius";
+            }
+            catch
+            {
+                return Error;
+            }
+
         }
-        public String toss_a_coin()
+        public String toss_a_coin() //toss coin
         {
             Random random = new Random();
             return heads_or_tails[random.Next(2)];
